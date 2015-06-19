8786ddaf93b5bcb35b30310da65411911c40c567
patrickmj <patrickmjchnm@gmail.com>
2015-06-18 22:10:54 -0400

    disable import vocabs when vocabs are already imported

--- src/Form/ConfigForm.php
+++ src/Form/ConfigForm.php
@@ -8,21 +8,45 @@ class ConfigForm extends AbstractForm
     public function buildForm()
     {
         $translator = $this->getTranslator();
-
+        $api = $this->getServiceLocator()->get('Omeka\ApiManager');
+        
+        $hasFedoraVocab = $api->read('vocabularies', array('namespace_uri' => 'http://fedora.info/definitions/v4/repository#' ));
+        if ($hasFedoraVocab) {
+            $info = $translator->translate('The Fedora Vocabulary is installed in Omeka');
+        } else {
+            $info = $translator->translate('Import the Fedora Vocabulary into Omeka.');
+        }
+        
         $this->add(array (
                         'type' => 'checkbox',
                         'name' => 'import_fedora',
                         'options' => array (
-                                    'label' => 'Import Fedora Vocabulary',
-                                    'info'  => 'Import the Fedora Vocabulary into Omeka'
+                                    'label' => $translator->translate('Import Fedora Vocabulary'),
+                                    'info'  => $info
+                                ),
+                        'attributes' => array (
+                                    'checked'  => $hasFedoraVocab ? 'checked' : '',
+                                    'disabled' => $hasFedoraVocab ? 'disabled' : ''
                                 )
                     ));
+
+        $hasLdpVocab = $api->read('vocabularies', array('namespace_uri' => 'http://www.w3.org/ns/ldp#' ));
+        if ($hasLdpVocab) {
+            $info = $translator->translate('The Linked Data Platform Vocabulary is installed in Omeka');
+        } else {
+            $info = $translator->translate('Import the Linked Data Platform Vocabulary into Omeka.');
+        }
+                
         $this->add(array (
                         'type' => 'checkbox',
                         'name' => 'import_ldp',
                         'options' => array (
-                                    'label' => 'Import Linked Data Platform Vocabulary',
-                                    'info'  => 'Import the Linked Data Platform Vocabulary into Omeka'
+                                    'label' => $translator->translate('Import Linked Data Platform Vocabulary'),
+                                    'info'  => $info
+                                ),
+                        'attributes' => array (
+                                    'checked'  => $hasLdpVocab ? 'checked' : '',
+                                    'disabled' => $hasLdpVocab ? 'disabled' : ''
                                 )
                     ));
     }
