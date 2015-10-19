70b0cef4e711b9867aed5b6573aa6bef82a39331
patrickmj <patrickmjchnm@gmail.com>
2015-10-19 11:32:15 -0400

    use messenger for import info
    fix #12

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -19,6 +19,7 @@ class IndexController extends AbstractActionController
                 $job = $dispatcher->dispatch('FedoraConnector\Job\Import', $data);
                 //the FedoraImport record is created in the job, so it doesn't
                 //happen until the job is done
+                $this->messenger()->addSuccess('Importing in Job ID ' . $job->getId());
                 $view->setVariable('job', $job);
             } else {
                 $this->messenger()->addError('There was an error during validation');
